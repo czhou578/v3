40b454cbdb1c579e5b52dce2f122b0ad3d852ddb
czhou578 <colizu2020@gmail.com>
2023-04-18 13:23:52 -0500

    added all needed pages

---
+++ app/pages/Headerbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HeaderBar() {
+  return (
+    <div className="header-bar">
+      <div className="header-item">About Me</div>
+      <div className="header-item">Education</div>
+      <div className="header-item">Experience</div>
+      <div className="header-item">Projects</div>
+      <div className="header-item">Designs</div>
+      <div className="header-item">Resume</div>
+    </div>
+  );
+}
+
+export default HeaderBar;
