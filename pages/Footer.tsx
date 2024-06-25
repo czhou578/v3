7b44a24c139a0a421c31ef5c470a9136b325b54b
czhou578 <colizu2020@gmail.com>
2024-06-25 12:15:04 -0700

    updated footer

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -1,62 +1,93 @@
 import Script from "next/script";
+import { SiLeetcode } from "react-icons/si";
+import { useState } from "react";
 
 export default function Footer() {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  const links = [
+    {
+      href: "https://github.com/czhou578",
+      icon: "fa-brands fa-github",
+      name: "GitHub",
+    },
+    {
+      href: "https://www.quora.com/profile/Colin-Zhou-6",
+      icon: "fa-brands fa-quora",
+      name: "Quora",
+    },
+    {
+      href: "https://www.linkedin.com/in/colin-z-84a1a0137/",
+      icon: "fa-brands fa-linkedin",
+      name: "LinkedIn",
+    },
+    {
+      href: "https://x.com/ColinZhouUSA",
+      icon: "fa-brands fa-twitter",
+      name: "Twitter",
+    },
+    {
+      href: "https://www.instagram.com/colin.pianist/",
+      icon: "fa-brands fa-instagram",
+      name: "Instagram",
+    },
+    {
+      href: "https://leetcode.com/u/colizu2020/",
+      icon: "leetcode",
+      name: "LeetCode",
+    },
+    {
+      href: "https://dribbble.com/colinzhou",
+      icon: "fa-brands fa-dribbble",
+      name: "Dribbble",
+    },
+    {
+      href: "https://www.worldcubeassociation.org/persons/2014ZHOU16",
+      icon: "fa-solid fa-cube",
+      name: "WCA",
+    },
+  ];
+
   return (
-    <footer>
+    <footer className="bg-gray-100 dark:bg-gray-800">
       <Script
         src="https://kit.fontawesome.com/8be8acb73b.js"
         crossOrigin="anonymous"
       />
-      <div className="mx-auto w-full max-w-screen-xl">
-        <div className="px-4 py-6 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-            © 2023 <a href="https://flowbite.com/">Colin Zhou</a>. All Rights
-            Reserved.
+      <div className="mx-auto w-full max-w-screen-xl px-4 py-6">
+        <div className="flex flex-col items-center space-y-4 sm:flex-row sm:justify-between sm:space-y-0">
+          <span className="text-sm text-gray-500 dark:text-gray-300 text-center sm:text-left">
+            © 2023-2024 <a href="https://flowbite.com/">Colin Zhou</a>. All
+            Rights Reserved.
           </span>
-          <div className="flex mt-4 space-x-6 sm:justify-center md:mt-0">
-            <a
-              href="https://github.com/czhou578"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <i className="fa-brands fa-github "></i>
-            </a>
-            <a
-              href="https://www.quora.com/profile/Colin-Zhou-6"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <i className="fa-brands fa-quora"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/colin-z-84a1a0137/"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-            <a
-              href="https://x.com/ColinZhouUSA"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <i className="fa-brands fa-twitter"></i>
-            </a>
-            <a
-              href="https://www.instagram.com/colin.pianist/"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <i className="fa-brands fa-instagram"></i>
-            </a>
-            {/* <a
-              href="https://www.worldcubeassociation.org/persons/2014ZHOU16"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <i className="fa-solid fa-cube"></i>
-            </a> */}
-            <a
-              href="https://dribbble.com/colinzhou"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <i className="fa-solid fa-cube"></i>
-              <span className="sr-only">Dribbble account</span>
-            </a>
+          <div className="flex flex-wrap justify-center gap-3 sm:gap-4">
+            {links.map((link, index) => (
+              <div
+                key={index}
+                className="relative"
+                onMouseEnter={() => setHoveredIndex(index)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <a
+                  href={link.href}
+                  className="text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-black transition-colors duration-300"
+                  aria-label={link.name}
+                >
+                  <div className="bg-blue-100 p-2 sm:p-3 rounded-full flex items-center justify-center w-10 h-10 sm:w-12 sm:h-12">
+                    {link.icon === "leetcode" ? (
+                      <SiLeetcode className="text-xl sm:text-2xl" />
+                    ) : (
+                      <i className={`${link.icon} text-xl sm:text-2xl`}></i>
+                    )}
+                  </div>
+                </a>
+                {hoveredIndex === index && (
+                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xs rounded whitespace-nowrap">
+                    {link.name}
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
